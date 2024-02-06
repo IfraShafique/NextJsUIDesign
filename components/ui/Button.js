b41573985a29e4e22c04372477299d2a766cc640
Ifra Shafique <86965873+IfraShafique@users.noreply.github.com>
2024-02-06 13:05:46 +0500

    Updated

--- components/ui/Button.js
+++ components/ui/Button.js
@@ -5,7 +5,7 @@ export default function Button({buttonText}) {
     <div>
       {/* button */}
       <div className="flex items-center justify-center  sm:mt-10 -mt-6">
-        <button className="xl:w-[25%] lg:w-[35%] sm:w-[40%] w-[65%] h-[8vh]  bg-[#FF8A36] rounded-lg border-black border-2 xl:text-2xl sm:text-[25px] text-normal font-semibold ">{buttonText}</button>
+        <button className="xl:w-[25%] lg:w-[35%] sm:w-[40%] w-[65%] h-[8vh]  bg-[#FF8A36] rounded-lg border-black border-2 xl:text-xl sm:text-[25px] text-normal font-semibold ">{buttonText}</button>
       </div>
     </div>
   )
